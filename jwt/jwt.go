27c15a568a3b0e547755b9144c39491f15a776fe
kanopeld <968597@gmail.com>
2020-01-22 11:22:39 +0300

    fix imort names

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -19,9 +19,9 @@ import (
 	"strings"
 	"time"
 
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/internal"
-	"golang.org/x/oauth2/jws"
+	"github.com/kanopeld/oauth2"
+	"github.com/kanopeld/oauth2/internal"
+	"github.com/kanopeld/oauth2/jws"
 )
 
 var (
